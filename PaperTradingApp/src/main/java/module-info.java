861e79c533925f8e1b3b5db1dae36802e8777e2f
pepariga <94783388+pepariga@users.noreply.github.com>
2023-04-28 02:33:00 +0200

    Delete PaperTradingApp directory

--- PaperTradingApp/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.papertradingapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.jsoup;
-
-
-    opens com.example.papertradingapp to javafx.fxml;
-    exports com.example.papertradingapp;
-}
